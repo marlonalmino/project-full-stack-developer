b5de215d756e0d3030f5be4223198dea6443f7ef
Marlon Almino <alminomarlon@gmail.com>
2021-10-21 16:38:24 -0300

    list functions implemented

--- Fundamentos/09 - API Countries/js/script.js
+++ Fundamentos/09 - API Countries/js/script.js
@@ -63,20 +63,64 @@ function renderCountryList() {
     const { name, flag, id, population} = country
 
     const countryHTML = `
-    <div class='country'>
-      <div>
-      </div>
-      
-      <div>
-      </div>
+      <div class='country'>
+        <div>
+          <a id="${id}" class="waves-effect waves light btn">+</a>
+        </div>
+        
+        <div>
+          <img src="${flag}" alt="${name}">
+        </div>
 
-      <div>
+        <div>
+          <ul>
+            <li>${name}</li>
+            <li>${population}</li>
+          </ul>
+        </div>
       </div>
-    </div>
     `
+
+    countriesHTML += countryHTML
   })
+
+  countriesHTML += "</div>"
+
+  tabCountries.innerHTML = countriesHTML
 }
 
-function renderFavorites() { }
+function renderFavorites() {
+  let favoriteHTML = "<div>"
+
+  favoriteCountries.forEach(country => {
+    const { name, flag, id, population} = country
+
+    const favoriteCountryHTML = `
+      <div class='country'>
+        <div>
+          <a id="${id}" class="waves-effect waves light btn red darken-4">-</a>
+        </div>
+        
+        <div>
+          <img src="${flag}" alt="${name}">
+        </div>
+
+        <div>
+          <ul>
+            <li>${name}</li>
+            <li>${population}</li>
+          </ul>
+        </div>
+      </div>
+    `
+
+    favoriteHTML += favoriteCountryHTML
+  })
+
+  favoriteHTML += "</div>"
+
+  tabFavorites.innerHTML = favoriteHTML
+}
+
 function renderSummary() { }
 function handleCountryButtons() { }
