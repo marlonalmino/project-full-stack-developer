62014e9c21d1280a556cdd426ce9184537ba697e
Marlon Almino <alminomarlon@gmail.com>
2021-10-27 17:02:09 -0300

    express - routes

---
+++ Desenvolvimento de APIs/03 - Express/NodeEx/index.js
@@ -0,0 +1,56 @@
+import express from "express"
+
+const app = express()
+// Serve para trabalharmos com json nas requisições
+//app.use(express.json)
+
+/* Requisições com Express */
+app.get('/', (_req, res) => {
+  res.send('Hello World GET!')
+})
+
+app.post('/', (_req, res) => {
+  res.send('Hello World POST!')
+})
+
+// Funcionará com qualquer método utilizado na requisição
+app.all('/testAll', (req, res) => {
+  res.send(req.method)
+})
+
+// Quando utilizado '?' ele pega o caminho ou o caminho menos a última letra
+// Ex: /test || /teste
+app.get('/teste?', (_req, res) => {
+  res.send('/teste?')
+})
+
+// Quando utilizado '+' pode repetir a última letra quantas vezes quiser
+// Ex: /buzz || /buzzzzzz || /buz (se retirar a letra dá erro)
+app.get('/buzz+', (_req, res) => {
+  res.send('/buzz+')
+})
+
+// Quanto utilizado '*' pode ser escrito qualquer coisa no intervalo
+// Ex: /oneAnythingBlue || /onesmmamskelbaBlue
+app.get('/one*Blue', (_req, res) => {
+  res.send('/one*Blue')
+})
+
+// Quando utilizado '()' o conjunto é tratado como 1 caractere
+// Ex: /test || /testing || /testin (esse não irá funcionar)
+app.post('/test(ing)?', (req, res) => {
+  console.log(req.body)
+  res.send('/test(ing)?')
+})
+
+// Expressões Regulares
+app.get(/.*Red$/, (_req, res) => {
+  res.send('/.*Red$/')
+})
+
+app.get('/testParam/:id', (req, res) => {
+  res.send(req.params.id)
+})
+
+// Criando o servidor
+app.listen(3000, () => console.log('API Started!'))
