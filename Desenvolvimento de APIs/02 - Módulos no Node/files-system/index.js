5f626bf40b3405795308a64029e6318f70a46903
Marlon Almino <alminomarlon@gmail.com>
2021-10-27 13:11:26 -0300

    files-system module with json

--- Desenvolvimento de APIs/02 - Módulos no Node/files-system/index.js
+++ Desenvolvimento de APIs/02 - Módulos no Node/files-system/index.js
@@ -3,12 +3,36 @@ import { promises as fs } from 'fs'
 // async/await
 
 init()
+writeReadJson()
 
 async function init() {
   try {
     await fs.writeFile('teste.txt', 'this is the text into txt')
     await fs.appendFile('teste.txt', '\nteste append file')
     const data = await fs.readFile('teste.txt', 'utf-8')
+    console.log(data)
+
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+async function writeReadJson() {
+  try {
+    // escrita om valores iniciais
+    const arrayCarros = ['Gol', 'Palio', 'Uno']
+    const obj = { carros: arrayCarros }
+    await fs.writeFile('teste.json', JSON.stringify(obj))
+
+    // leitura do conteudo atual
+    const data = JSON.parse(await fs.readFile('teste.json'))
+    console.log(data)
+
+    // conteudo modificado
+    data.carros.push('Sandero')
+
+    // sobrescrever o arquivo com o conteudo modificado
+    await fs.writeFile('teste.json', JSON.stringify(data))
     console.log(data)
 
   } catch (err) {
