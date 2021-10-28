0a5a1109d3460944f5b3f89fea6bb7cffbaeae98
Marlon Almino <alminomarlon@gmail.com>
2021-10-28 16:57:58 -0300

    my bank api - initial commit

---
+++ Desenvolvimento de APIs/MY-BANK-API/index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import accountsRouter from './routes/accounts.js'
+import { promises as fs, readFileSync } from 'fs'
+
+const { readFile, writeFile } = fs
+
+const app = express()
+app.use(express.json())
+
+app.use('/account', accountsRouter)
+
+app.listen(3000, async () => {
+  try {
+    await readFile('accounts.json')
+  } catch (err) {
+    const initialJson = {
+      nextId: 1,
+      accounts: []
+    }
+    writeFile('accounts.json', JSON.stringify(initialJson)).then(() => {
+      console.log('API Started and File Created!')
+    }).catch(err => console.log(err))
+  }
+})
