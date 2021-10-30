b21bc07bd338674fd63677025de4ddf92f6ffdef
Marlon Almino <alminomarlon@gmail.com>
2021-10-30 05:08:35 -0300

    created delete method

--- Desenvolvimento de APIs/MY-BANK-API/routes/accounts.js
+++ Desenvolvimento de APIs/MY-BANK-API/routes/accounts.js
@@ -48,4 +48,18 @@ router.get('/:id', async (req, res) => {
   }
 })
 
+router.delete('/:id', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName))
+    data.accounts = data.accounts.filter(
+      account => account.id !== parseInt(req.params.id))
+
+    await writeFile(global.fileName, JSON.stringify(data, null, 2))
+
+    res.end()
+  } catch (err) {
+    res.status(400).send({ error: err.message })
+  }
+})
+
 export default router
