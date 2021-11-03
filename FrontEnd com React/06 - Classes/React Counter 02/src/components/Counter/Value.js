f48bbffc67884956e5eda33e8917fe3727abfd71
Marlon Almino <alminomarlon@gmail.com>
2021-11-03 16:04:56 -0300

    React Counter 02 - shared states with props

--- FrontEnd com React/06 - Classes/React Counter 02/src/components/Counter/Value.js
+++ FrontEnd com React/06 - Classes/React Counter 02/src/components/Counter/Value.js
@@ -4,7 +4,6 @@ import css from './counter.module.css'
 
 export default class Value extends Component {
   render() {
-    console.log(this.props)
     return (
       <span className={css.counterValue}>{this.props.value}</span>
     )
