db60007344c8e8fac7e1633edf57bd37f4f4fe86
Marlon Almino <alminomarlon@gmail.com>
2021-11-05 21:31:33 -0300

    RFP - implemented Position and Picture

--- FrontEnd com React/10 - React Functional/frontend/src/components/Candidate.js
+++ FrontEnd com React/10 - React Functional/frontend/src/components/Candidate.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Picture from './Picture'
+import Position from './Position'
 
-export default function Candidate({ candidate }) {
-  const { name, votes } = candidate
+export default function Candidate({ candidate, position }) {
+  const { id, name, votes } = candidate
+
+  const imageSource = `${id}.jpg`
 
   return (
     <div>
+      <Position>{position}</Position>
+      <Picture imageSource={imageSource} description={name} />
       {name} - {votes}
     </div>
   )
