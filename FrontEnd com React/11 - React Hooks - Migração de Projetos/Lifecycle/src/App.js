022cc8c257d70b9376f454b9546b778616f2c790
Marlon Almino <alminomarlon@gmail.com>
2021-11-09 17:42:42 -0300

    Project Lifecycle - Migration to Hooks

--- FrontEnd com React/11 - React Hooks - Migração de Projetos/Lifecycle/src/App.js
+++ FrontEnd com React/11 - React Hooks - Migração de Projetos/Lifecycle/src/App.js
@@ -1,40 +1,38 @@
-import React, { Component } from 'react';
-import Toggle from './components/toggle/Toggle';
-import Users from './components/users/Users';
+import React, { useState, useEffect } from 'react'
+import Toggle from './components/toggle/Toggle'
+import Users from './components/users/Users'
 
-export default class App extends Component {
-  constructor() {
-    super()
+export default function App() {
+  const [users, setUsers] = useState([])
+  const [showUsers, setShowUsers] = useState(false)
 
-    this.state = {
-      users: [],
-      showUsers: false,
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await fetch(
+        'https://randomuser.me/api/?seed=rush&nat=br&results=10',
+      )
+
+      const json = await res.json()
+      setUsers(json.results)
     }
+
+    fetchUsers()
+  }, [])
+
+  const handleShowUsers = (isChecked) => {
+    setShowUsers(isChecked)
   }
 
-  async componentDidMount() {
-    const res = await fetch('https://randomuser.me/api/?seed=rush&nat=br&results=10')
-    const json = await res.json()
-
-    this.setState({
-      users: json.results
-    })
-  }
-
-  handleShowUsers = (isChecked) => {
-    this.setState({ showUsers: isChecked })
-  }
-
-  render() {
-    const { showUsers, users } = this.state
-
-    return (
-      <div>
-        <h3>React Lifecycle</h3>
-        <Toggle description='Mostrar usuários:' enabled={showUsers} onToggle={this.handleShowUsers} />
-        <hr />
-        {showUsers && <Users users={users} />}
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h3>React Lifecycle</h3>
+      <Toggle
+        description="Mostrar usuários:"
+        enabled={showUsers}
+        onToggle={handleShowUsers}
+      />
+      <hr />
+      {showUsers && <Users users={users} />}
+    </div>
+  )
 }
--- FrontEnd com React/11 - React Hooks - Migração de Projetos/Lifecycle/src/components/users/Users.js
+++ FrontEnd com React/11 - React Hooks - Migração de Projetos/Lifecycle/src/components/users/Users.js
@@ -1,54 +1,31 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import User from './User'
 
-export default class Users extends Component {
-  constructor() {
-    super()
+export default function Users({ users }) {
+  const [secondsVisible, setSecondsVisible] = useState(0)
 
-    this.state = {
-      secondsVisible: 0,
-    }
-
-    this.interval = null
-  }
-
-  componentDidMount() {
-    console.log('componentDidMount de Users.js')
-
-    this.interval = setInterval(() => {
-      const { secondsVisible } = this.state
-      this.setState({
-        secondsVisible: secondsVisible + 1
-      })
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSecondsVisible(secondsVisible + 1)
     }, 1000)
-  }
+    return () => {
+      clearInterval(interval)
+    }
+  }, [secondsVisible])
 
-  componentDidUpdate() {
-    console.log('componentDidUpdate de Users.js')
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval)
-  }
-
-  render() {
-    const { users } = this.props
-    const { secondsVisible } = this.state
-
-    return (
-      <div>
-        <p>Componente Users visível por {secondsVisible}</p>
-        <ul>
-          {users.map(user => {
-            const { login } = user
-            return (
-              <li key={login.uuid}>
-                <User user={user} />
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <p>Componente Users visível por {secondsVisible}</p>
+      <ul>
+        {users.map((user) => {
+          const { login } = user
+          return (
+            <li key={login.uuid}>
+              <User user={user} />
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
