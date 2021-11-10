30e9e07b9f61f99a82e9f102204e2a4b1bf5b6d2
Marlon Almino <alminomarlon@gmail.com>
2021-11-10 18:39:32 -0300

    RHP - implemented async method getAllGrades()

--- FrontEnd com React/12 - React Hooks - Composição de Notas/frontend/src/api/apiService.js
+++ FrontEnd com React/12 - React Hooks - Composição de Notas/frontend/src/api/apiService.js
@@ -38,6 +38,67 @@ async function getAllGrades() {
     }
   })
 
+  let allStudents = new Set()
+  grades.forEach((grade) => allStudents.add(grade.student))
+  allStudents = Array.from(allStudents)
+
+  let allSubjects = new Set()
+  grades.forEach((grade) => allSubjects.add(grade.subject))
+  allSubjects = Array.from(allSubjects)
+
+  let allGradeTypes = new Set()
+  grades.forEach((grade) => allGradeTypes.add(grade.type))
+  allGradeTypes = Array.from(allGradeTypes)
+
+  let maxId = -1
+  grades.forEach(({ id }) => {
+    if (id > maxId) {
+      maxId = id
+    }
+  })
+
+  let nextId = maxId + 1
+  const allCombinations = []
+  allStudents.forEach((student) => {
+    allSubjects.forEach((subject) => {
+      allGradeTypes.forEach((type) => {
+        allCombinations.push({
+          student,
+          subject,
+          type,
+        })
+      })
+    })
+  })
+
+  allCombinations.forEach(({ student, subject, type }) => {
+    const hasItem = grades.find((grade) => {
+      return (
+        grade.subject === subject &&
+        grade.student === student &&
+        grade.type === type
+      )
+    })
+
+    if (!hasItem) {
+      grades.push({
+        id: nextId++,
+        student,
+        studentLowerCase: student.toLowerCase(),
+        subject,
+        subjectLowerCase: subject.toLowerCase(),
+        type,
+        typeLowerCase: type.toLowerCase(),
+        value: 0,
+        isDeleted: true,
+      })
+    }
+  })
+
+  grades.sort((a, b) => a.typeLowerCase.localeCompare(b.typeLowerCase))
+  grades.sort((a, b) => a.subjectLowerCase.localeCompare(b.subjectLowerCase))
+  grades.sort((a, b) => a.studentLowerCase.localeCompare(b.studentLowerCase))
+
   return grades
 }
 
