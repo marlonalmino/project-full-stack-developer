1ddfcb3ce1720d1680cd3d7c56be2a566ad64045
Marlon Almino <alminomarlon@gmail.com>
2021-11-24 11:05:51 -0300

    Insert document with Mongoose

--- Database/02 - Mongoose/s_mongoose/index.js
+++ Database/02 - Mongoose/s_mongoose/index.js
@@ -8,3 +8,46 @@ await mongoose.connect(
     useUnifiedTopology: true,
   },
 )
+
+// Criando modelo
+const studentSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+
+  subject: {
+    type: String,
+    require: true,
+  },
+
+  type: {
+    type: String,
+    require: true,
+  },
+
+  value: {
+    type: Number,
+    require: true,
+  },
+
+  lastModified: {
+    type: Date,
+    default: Date.now(),
+  },
+})
+
+// Definindo o modelo da coleção
+mongoose.model('student', studentSchema, 'student')
+
+const student = mongoose.model('student')
+
+new student({
+  name: 'Marlon Almino',
+  subject: 'Matemática',
+  type: 'Trabalho Prático',
+  value: 22,
+})
+  .save()
+  .then(() => console.log('Documento inserido'))
+  .catch((err) => console.log('Falha ao inserir documento', err))
